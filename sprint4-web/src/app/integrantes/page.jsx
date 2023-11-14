2ed273014373084a8a2103976e7be5230327d2ec
IgorRibeiro7 <igorbez.12@icloud.com>
2023-11-13 23:11:22 -0300

    Rm modificado

--- sprint4-web/src/app/integrantes/page.jsx
+++ sprint4-web/src/app/integrantes/page.jsx
@@ -1,6 +1,6 @@
 export default function Integrantes(){
- 
- 
+
+
  
     return(
  
@@ -8,11 +8,11 @@ export default function Integrantes(){
             <h1>TheBikers</h1>
  
             <div className='nomeIntegrante'>
-                <p>Enzo Ross Gallone - RM551754</p>
-                <p>Igor Bezerra - RM550989</p>
-                <p>Leonardo Scotti - RM550769</p>
-                <p>Mariana Albano - RM551154</p>
-                <p>Vitor Kubica - RM98903</p>
+                <p>Enzo Ross Gallone - rm551754</p>
+                <p>Igor Bezerra - rm550989</p>
+                <p>Leonardo Scotti - rm550769</p>
+                <p>Mariana Albano - rm551154</p>
+                <p>Vitor Kubica - rm98903</p>
                 <p><a href="https://github.com/gallone10/sprint4-html" target='_blank' rel="noreferrer">Link para o repositório</a></p>
             </div>
            
