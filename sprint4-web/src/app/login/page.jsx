6aad6578024df0c2b0928d573c86dda04200ea56
VitorKubica <vitor05kubica12@gmail.com>
2023-11-13 23:48:11 -0300

    crição do formulario

--- sprint4-web/src/app/login/page.jsx
+++ sprint4-web/src/app/login/page.jsx
@@ -11,7 +11,30 @@ export default function Home(){
             <div>
                 <Menu></Menu>
                 <Image src={LogoPorto} alt="LogoPorto" className={roberto.Logo}/>
-                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                <fieldset>
+                <legend>Login de Usuários</legend>
+                <div>
+                <label htmlFor="idCPF">CPF</label>
+                <input type="String" name="cpf" id="idECPF" placeholder="Digite seu CPF." value={usuario.cpf} onChange={handleChange}/>
+                </div>
+                <div>
+                <label htmlFor="idNome">Nome</label>
+                <input type="String" name="nome" id="idNome" placeholder="Digite seu Nome." value={usuario.nome} onChange={handleChange}/>
+                </div>
+                <div>
+                <label htmlFor="idEmail">E-mail</label>
+                <input type="email" name="email" id="idEmail" placeholder="Digite seu E-mail." value={usuario.email} onChange={handleChange}/>
+                </div>
+                <div>
+                <label htmlFor="idSenha">Senha</label>
+                <input type="password" name="senha" id="idSenha" placeholder="Digite sua Senha." value={usuario.senha} onChange={handleChange}/>
+                </div>
+                <div>
+                <button>Login</button>
+                </div>
+                </fieldset>
+                </form>
             </div>
         </main>
     )
