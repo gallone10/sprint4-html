4916e27c54281d81f9868b132ea29da146593870
VitorKubica <vitor05kubica12@gmail.com>
2023-11-13 23:10:07 -0300

    criação do formulário

--- sprint4-web/src/app/login/page.jsx
+++ sprint4-web/src/app/login/page.jsx
@@ -10,6 +10,22 @@ export default function Home(){
                 <Menu></Menu>
                 <Image src={LogoPorto} alt="LogoPorto" className='LogoPorto'/>
                 <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                <fieldset>
+                <legend>Login de Usuários</legend>
+                <div>
+                <label htmlFor="idEmail">E-mail</label>
+                <input type="email" name="email" id="idEmail" placeholder="Digite seu E-mail." value={usuario.email} onChange={handleChange}/>
+                </div>
+                <div>
+                <label htmlFor="idSenha">Senha</label>
+                <input type="password" name="senha" id="idSenha" placeholder="Digite sua Senha." value={usuario.senha} onChange={handleChange}/>
+                </div>
+                <div>
+                <button>Login</button>
+                </div>
+                </fieldset>
+                </form>
             </div>
          
            
